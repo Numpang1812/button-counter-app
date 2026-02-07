8792eca3ed37713287d6f3621c08a095d330fda3
Numpang <thenumpang@gmail.com>
2026-02-07 14:34:40 +0700

    Added simple styling

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,12 +1,16 @@
 <script>
 	import { updateCounter, initializeCounter, counterIncrement } from "../utils/logic";
+    import "../styles/global.css";
     initializeCounter();
     updateCounter();
 </script>
-<h1>Welcome to Button Counter App</h1>
-<div>
+<div class="header-container">
+    <h1>Welcome to Button Counter App</h1>
+</div>
+<div class="counter-container">
     <p>Here is the current counter: </p>
     <span id="counter">0</span>
     <button onclick={counterIncrement}>Increase Counter</button>
 </div>
+
 
