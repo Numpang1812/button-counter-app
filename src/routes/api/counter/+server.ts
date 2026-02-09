a7fbebeca087d32329bded412f02eeb8cdbfde3d
Monineathh <143490227+Monineathh@users.noreply.github.com>
2026-02-09 23:52:31 +0700

    Create a decrement function #20

--- src/routes/api/counter/+server.ts
+++ src/routes/api/counter/+server.ts
@@ -1,13 +1,26 @@
-import { json } from '@sveltejs/kit';
-import { getCurrentCounter, incrementCounter } from '../../../database/db.server';
+import { json } from "@sveltejs/kit";
+import {
+  getCurrentCounter,
+  incrementCounter,
+  decrementCounter
+} from "../../../database/db.server";
 
 export async function GET() {
   const value = await getCurrentCounter();
   return json({ counter: value });
 }
 
-export async function POST() {
-  await incrementCounter();
+export async function POST({ request }) {
+  const { action } = await request.json();
+
+  if (action === "increment") {
+    await incrementCounter();
+  }
+
+  if (action === "decrement") {
+    await decrementCounter();
+  }
+
   const value = await getCurrentCounter();
   return json({ counter: value });
 }
